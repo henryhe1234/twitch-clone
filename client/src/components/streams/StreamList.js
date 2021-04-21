1862d3ad3e5d0569a5a91e8dec9f928f0d72d68c
henryhe1234 <henryhe1123@gmail.com>
2021-04-21 16:11:03 -0600

    fetching stream and show the title and description

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -16,7 +16,8 @@ class StreamList extends React.Component {
           </Link>
           <Link
             to={`/streams/delete/${stream.id}`}
-            className="ui button negative">
+            className="ui button negative"
+          >
             Delete
           </Link>
         </div>
@@ -32,7 +33,8 @@ class StreamList extends React.Component {
           {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content">
-            {stream.title}
+            <Link to={`/streams/${stream.id}`}>{stream.title}</Link>
+
             <div className="description">{stream.description}</div>
           </div>
         </div>
